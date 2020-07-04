f062d43fb721809d761a7b7704a17b6008f84523
Amit Gupta <agupta@thinkanalytics.com>
2020-07-04 15:34:25 +0530

    feat: Initial Commit

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider{
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String pwd = authentication.getCredentials().toString();
+		
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
